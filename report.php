e32a517639ffe5c7f3a49ab896079e25c55d985b
Marcos <marcos_alexandre87@yahoo.com.br>
2017-05-17 22:42:47 -0300

    Revert "Adding table on report"
    This reverts commit ce5cdf7761779c152535486c6b6fdb37244011b1.

--- report.php
+++ report.php
@@ -10,43 +10,23 @@
 $filename = "database.txt";
 $handle = fopen($filename, "r");
 
-<div class="ui centered grid">
-  <div class="row">
-    <div class="ui fourteen wide column">
-      <table class="ui celled table">
-        <thead>
-          <tr>
-            <th>Nome de Prestador de Serviço</th>
-            <th>Serviço Contratado</th>
-            <th>Nome Usuário</th>
-            <th>Senha Usuário</th>
-            <th>Utilizador da Senha</th>
-            <th>Link Serviço</th>
-          </tr>
-        </thead>
-        <tbody>
-
-  while ($line = fgets($handle)) {
-    echo "<tr>"
+while ($line = fgets($handle)) {
     $columns = split(";", trim($line));
 
     foreach ($columns as &$value) {
-        echo "<td>$value</td>";
+        echo "$value ";
     }
 
-    echo "</tr>"
     echo "<br/>";
 }
-      </tbody>
-    </table>
-    </div>
-  </div>
-</div>
+
 fclose($handle);
 
 ?>
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.10/semantic.min.css">
-
 
 </body>
 </html>
+
+
+
+
