fefdf0bbc87e40e8c1b2464228425fb7a7f0ceee
Bruno Hirata <bhirata@br.ibm.com>
2017-05-17 21:59:15 -0300

    Adicionado a tela para gerenciar acesso.

---
+++ GerenciarAcesso.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Colaborador</title>
+</head>
+<body>
+	<form action="">
+		Nome de acesso: <input type="text" name="nome"><br>
+		Senha: <input type="text" name="senha"><br>
+		<input type="submit" value="Cadastrar">
+	</form>
+
+	<form action="" style="margin-top: 35px;">
+		Exlcluir acesso: <input type="text" name="ex_nome"><br>
+		<input type="submit" name="Exlcluir">
+	</form>
+</body>
+</html>
